884c41011e054dd097d18b7d9e6882a5b714fc0e
elisioo <e.sorono.544027@umindanao.edu.ph>
2025-03-19 17:28:23 +0800

    layout

---
+++ resource/layout/web-layout.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Enrollment System</title>
+
+    <link rel="stylesheet" href="../css/layout.css" />
+    <link rel="stylesheet" href="../css/sidebar.css" />
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link rel="stylesheet" href="../statics/css/bootstrap.min.css">
+
+</head>
+
+<body>
+    <header>
+        <div class="logo-container">
+            <img src="../img/logo.png" alt="logo" class="logo" />
+        </div>
+        <div class="vl"></div>
+        <h2>Enrollment System</h2>
+        <div class="profile-container">
+            <i class="fa-solid fa-circle-user"></i>
+            <p>Admin</p>
+        </div>
+    </header>
+    <?php include '../views/sidebar.php' ?>
+    <main>
+        <h2>Welcome to the Enrollment System</h2>
+        <p>This is the main content area.</p>
+    </main>
+
+    <script src="../statics/js/bootstrap.bundle.js"></script>
+</body>
+
+</html>
